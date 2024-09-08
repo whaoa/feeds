6a2f2cb6df2939c389565f9551c25e1c06304a7c
whaoa <whaoa.w@outlook.com>
2024-09-08 23:47:36 +0800

    chore: init project

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main>
+      <h1 className="py-7 font-bold text-center text-lg text-slate-800">
+        Welcome to Next.js App
+      </h1>
+    </main>
+  );
+}
